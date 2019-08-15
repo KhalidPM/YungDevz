91f58cee5462a8bf7d93b64b6481741a9fbee5ea
Zyad <zyadgohary@hotmail.com>
2019-08-14 21:52:24 -0700

    Finalized firebase configs

--- Dev/android/app/src/main/java/com/quranconnect/MainApplication.java
+++ Dev/android/app/src/main/java/com/quranconnect/MainApplication.java
@@ -6,9 +6,13 @@ import android.util.Log;
 import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
+import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -27,6 +31,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
+      packages.add(new RNFirebaseAuthPackage());
+      packages.add(new RNFirebasePackage());
+      packages.add(new RNFirebaseFirestorePackage());
+      packages.add(new RNFirebaseAnalyticsPackage());
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
       return packages;
